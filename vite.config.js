e149ae27ccf44c321c1d4ae93e5c8612d7002aca
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2024-09-15 16:08:12 +0300

    cjsjjsjsjjsjsjsjs

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,37 @@
 import { defineConfig } from "vite";
-import { glob } from "glob";
-import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
-import SortCss from "postcss-sort-media-queries";
+import path from "path"; // Додано імпорт модуля path
 
-export default defineConfig(async ({ command }) => {
-  const inputFiles = await glob("./src/*.html");
-
-  return {
-    // Вказуємо базовий шлях для коректної роботи на GitHub Pages
-    base: "/js-009/", // Замініть на назву вашого репозиторію
-    define: {
-      [command === "serve" ? "global" : "_global"]: {},
-    },
-    root: "src",
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: inputFiles.length ? inputFiles : "./src/index.html",
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return "vendor";
-            }
-          },
-          entryFileNames: "[name].js",
-          assetFileNames: (assetInfo) => {
-            if (assetInfo.name && assetInfo.name.endsWith(".html")) {
-              return "[name].[ext]";
-            }
-            return "assets/[name]-[hash][extname]";
-          },
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/js-009/" : "/",
+  root: "src",
+  build: {
+    sourcemap: true,
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/index.html"), // Оновлено шлях до index.html
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+        entryFileNames: "[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".html")) {
+            return "[name].[ext]";
+          }
+          return "assets/[name]-[hash][extname]";
         },
       },
-      outDir: "../dist",
-      emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(["./src/**/*.html"]),
-      SortCss({
-        sort: "mobile-first",
-      }),
-    ],
-    optimizeDeps: {
-      include: ["vite-plugin-html-inject", "vite-plugin-full-reload"],
-      exclude: ["fsevents"],
-    },
-    logLevel: "info",
-  };
-});
+  },
+  define: {
+    [command === "serve" ? "global" : "_global"]: {},
+  },
+  plugins: [],
+  optimizeDeps: {
+    exclude: ["fsevents"],
+  },
+  logLevel: "info",
+}));
